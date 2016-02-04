4f4465a9b6605d9bd6a5d81f73154e4a1fc26496
Luís Melo <luismelo7@gmail.com>
2016-02-04 23:12:26 +0000

    Zipline Completed v1

--- scripts/main.js
+++ scripts/main.js
@@ -5,49 +5,59 @@ $( document ).ready(function() {
   var PAUSE = 3;
   var stopMessage = "Come On, Click To Start Working... You Can do It!";
   var workMessage = "Put your hands to work... Work Hard!";
-  var breakMessage = "Time to Relax... You deserve it";
-  var pauseMessage = "Work Paused";
+  var breakMessage = "Time to Relax... You deserve it!";
+  var pauseMessage = "Pomodoro Paused!";
 
   var state = STOP;
 
   var clock = $('.clock').FlipClock({
   	  clockFace: 'MinuteCounter',
       countdown: true,
-      stop: function(){
-        if ( state == WORK && clock.getTime() == 0)
-        {
-          state = BREAK;
-          this.setTime(document.getElementById("breakLength").innerHTML*60);
-          this.start();
-          $('#message').html(breakMessage);
-        }
-        else if ( state == BREAK && clock.getTime() == 0)
-        {
-          state = WORK;
-          this.setTime(document.getElementById("workLength").innerHTML*60);
-          this.start();
-          $('#message').html(workMessage);
+      callbacks: {
+        stop: function() {
+          switchTempos()
         }
       }
   	});
 
+    var switchTempos = function(){
+      if ( state == WORK && clock.getTime() == 0)
+      {
+        state = BREAK;
+        clock.setTime(document.getElementById("breakLength").innerHTML*60);
+        clock.start();
+        $('#message').html(breakMessage);
+      }
+      else if ( state == BREAK && clock.getTime() == 0)
+      {
+        state = WORK;
+        clock.setTime(document.getElementById("workLength").innerHTML*60);
+        clock.start();
+        $('#message').html(workMessage);
+      }
+    };
+
+
+    console.log(clock);
   clock.setTime(25*60);
 
   $( ".changeWork" ).click(function(){
-    if (document.getElementById("workLength").innerHTML > 1)
+    if (document.getElementById("workLength").innerHTML >= 1)
     {
       var t = document.getElementById("workLength").innerHTML + this.innerHTML + "1";
-      if ( state == STOP)
+      if ( state == STOP && eval(t)!=0)
         clock.setTime(eval(t)*60);
-      document.getElementById("workLength").innerHTML = eval(t);
+      if (eval(t)!=0)
+          document.getElementById("workLength").innerHTML = eval(t);
     }
   });
 
   $( ".changeBreak" ).click(function(){
-    if (document.getElementById("breakLength").innerHTML > 1)
+    if (document.getElementById("breakLength").innerHTML >= 1)
     {
       var t = document.getElementById("breakLength").innerHTML + this.innerHTML + "1";
-      document.getElementById("breakLength").innerHTML = eval(t);
+      if (eval(t)!=0)
+        document.getElementById("breakLength").innerHTML = eval(t);
     }
   });
 
@@ -63,7 +73,7 @@ $( document ).ready(function() {
   });
 
   $( ".buttonpause" ).click(function(){
-    if (state == WORK)
+    if (state == WORK || state == "BREAK")
     {
       state = PAUSE;
       $(".buttonplay").removeClass("hidden");
